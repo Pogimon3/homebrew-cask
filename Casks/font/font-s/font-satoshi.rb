d5d2a830c4c4b02327fcae8d9cab76641a1087b6
Pogimon3 <140201901+Pogimon3@users.noreply.github.com>
2024-08-09 16:19:08 +0530

    Create font-satoshi.rb

---
+++ Casks/font/font-s/font-satoshi.rb
@@ -0,0 +1,22 @@
+cask "font-satoshi" do
+  version "1.0"
+  sha256 "044e5689b4b8b477f05d2bc4d1561e29215c20fecbcc5d884c71c15296c539dc"
+
+  url "https://api.fontshare.com/v2/fonts/download/satoshi",
+      verified: "fontshare.com"
+  name "Satoshi"
+  homepage "https://www.fontshare.com/fonts/satoshi"
+
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-BlackItalic.otf"
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-Bold.otf"
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-BoldItalic.otf"
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-Italic.otf"
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-Light.otf"
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-LightItalic.otf"
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-Medium.otf"
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-MediumItalic.otf"
+  font "Satoshi_Complete/Fonts/OTF/Satoshi-Regular.otf"
+  font "Satoshi_Complete/Fonts/TTF/Satoshi-Variable.ttf"
+  font "Satoshi_Complete/Fonts/TTF/Satoshi-VariableItalic.ttf"
+
+end
